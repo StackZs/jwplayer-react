a4d1ec4578d30191c3656991656e04a478d738e3
Eduard Mazur <stackzs@MacBook-Pro-Eduard.local>
2025-02-24 16:10:15 +0200

    Update Webpack // Per dependecies

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: './src/jwplayer.jsx',
-    output: {
-        path: path.resolve('lib'),
-        filename: 'jwplayer-react.js',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-                use: 'babel-loader'
-            }
-        ]
-    }
-}
+  mode: "production",
+  entry: "./src/jwplayer.jsx",
+  output: {
+    path: path.resolve("lib"),
+    filename: "jwplayer-react.js",
+    libraryTarget: "commonjs2",
+  },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+    "react/jsx-runtime": "react/jsx-runtime",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
